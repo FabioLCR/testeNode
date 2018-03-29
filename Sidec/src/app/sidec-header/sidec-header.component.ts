9dd4ef6fced2e712c1659e0c4337c244016deecd
Fabio Ribeiro <fribeiro@img.com.br>
2018-03-29 13:26:39 -0300

    logout

--- Sidec/src/app/sidec-header/sidec-header.component.ts
+++ Sidec/src/app/sidec-header/sidec-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidec-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidecHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private kc: KeycloakService) { }
+
+  logout() {
+    this.kc.logout();
+  }
 
   ngOnInit() {
   }
